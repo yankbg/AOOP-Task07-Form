d93a72c598cba8ceb0bc4200c07ab5803c219335
YANNICK kabunga <yankbg2@gmail.com>
2025-11-15 13:55:35 +0300

    refactor Terraform configuration to use local files for deployment tracking and application settings

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,21 @@
 terraform {
   required_version = ">= 1.0.0" # Ensure that the Terraform version is 1.0.0 or higher
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws" # Specify the source of the AWS provider
-      version = "~> 4.0"        # Use a version of the AWS provider that is compatible with version
-    }
-  }
 }
 
 provider "aws" {
   region = "us-east-1" # Set the AWS region to US East (N. Virginia)
 }
 
-resource "aws_instance" "aws_example" {
-  tags = {
-    Name = "ExampleInstance" # Tag the instance with a Name tag for easier identification
-  }
+resource "local_file" "jenkins_test" {
+  content  = "Infrastructure provisioned by Jenkins on ${timestamp()}"
+  filename = "${path.module}/jenkins-deployed.txt"
+}
+resource "local_file" "app_config" {
+  content = jsonencode({
+    app_name    = "Form Application"
+    environment = "development"
+    version     = "1.0"
+    deployed_by = "Jenkins"
+  })
+  filename = "${path.module}/config.json"
 }
